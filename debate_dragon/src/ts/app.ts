a7f8a83408e35821543da6fb6ec755f7c658855f
Shawn Yu <shawn.yu5@hotmail.com>
2022-02-05 15:19:32 -0500

    file clean up

--- debate_dragon/src/ts/app.ts
+++ debate_dragon/src/ts/app.ts
@@ -4,44 +4,44 @@ const utils = require("./utils");
 require("dotenv").config();
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
 client.on("ready", () => {
-  console.log(`${client.user.tag} logged in`);
+   console.log(`${client.user.tag} logged in`);
 });
 
 // command: $dd
 client.on("messageCreate", async (message: typeof Message) => {
-  let command = "$dd";
-  let content = message.content;
-  // make sure the bot didn't send the message. And the message starts with command
-  if (message.author.bot || !content.startsWith(command)) return;
+   let command = "$dd";
+   let content = message.content;
+   // make sure the bot didn't send the message. And the message starts with command
+   if (message.author.bot || !content.startsWith(command)) return;
 
-  // console.log(content);
-  content = utils.removeCommand(command, content);
+   // console.log(content);
+   content = utils.removeCommand(command, content);
 
-  await utils.textOverlay(content);
+   await utils.textOverlay(content);
 
-  await message.channel
-    .send({
-      files: [
-        {
-          attachment: "media/img/done.png",
-          name: "dragon.png",
-          description: "Debate dragon",
-        },
-      ],
-    })
-    .then(() => {
-      console.log("Image sent");
-      fs.unlink("media/img/done.png", (error: any) => {
-        console.log(error);
+   await message.channel
+      .send({
+         files: [
+            {
+               attachment: "media/img/done.png",
+               name: "dragon.png",
+               description: "Debate dragon",
+            },
+         ],
+      })
+      .then(() => {
+         console.log("Image sent");
+         fs.unlink("media/img/done.png", (error: any) => {
+            console.log(error);
+         });
+      })
+      .catch((error: any) => {
+         console.log(`Image failed to send: ${error}`);
       });
-    })
-    .catch((error: any) => {
-      console.log(`Image failed to send: ${error}`);
-    });
 });
 
 client.login(process.env.token);
