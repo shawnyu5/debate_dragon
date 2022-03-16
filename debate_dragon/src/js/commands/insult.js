72cd8e2ad8d4d628bb02d368e0cdb7f561380100
shawn yu <shawn.yu5@hotmail.com>
2022-03-15 22:56:17 -0400

    refactor(debate dragon): changed bot description

--- debate_dragon/src/js/commands/insult.js
+++ debate_dragon/src/js/commands/insult.js
@@ -18,7 +18,10 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("insult")
         .setDescription("Ping someone and insult them")
-        .addUserOption((option) => option.setName("user").setDescription("A string").setRequired(true)),
+        .addUserOption((option) => option
+        .setName("user")
+        .setDescription("Tag someone to insult them")
+        .setRequired(true)),
     async execute(interaction) {
         let author = getAuthor(String(interaction));
         // let userMessage = interaction.options._hoistedOptions[0].value;
