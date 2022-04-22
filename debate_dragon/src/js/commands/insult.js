4361115a485900af36319804ca6fcaa961d136ea
shawn yu <shawn.yu5@hotmail.com>
2022-04-21 21:57:07 -0400

    feat(debate dragon): If I am being insulted, insult the person running the command instead

--- debate_dragon/src/js/commands/insult.js
+++ debate_dragon/src/js/commands/insult.js
@@ -3,19 +3,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const { SlashCommandBuilder } = require("@discordjs/builders");
+const builders_1 = require("@discordjs/builders");
 const axios_1 = __importDefault(require("axios"));
+/**
+ * gets an insult from the API
+ * @returns {Promise<string>} Returns the insult from the API
+ */
 async function getInsult() {
     // get insult back in plain text
     let response = await axios_1.default.get("https://evilinsult.com/generate_insult.php?lang=en");
     let insult = response.data;
+    // check if response contains invalid words
+    if (insult.includes("&quot;")) {
+        await getInsult();
+    }
     return insult;
 }
-function getAuthor(interaction) {
-    return interaction.split(":")[1];
+/**
+ * get the user that is being insulted
+ * @param interaction - the interaction object
+ * @returns the id of the user tagged in the message
+ */
+function getInsultedUser(interaction) {
+    return String(interaction).split(":")[1];
 }
 module.exports = {
-    data: new SlashCommandBuilder()
+    data: new builders_1.SlashCommandBuilder()
         .setName("insult")
         .setDescription("Ping someone and insult them")
         .addUserOption((option) => option
@@ -23,7 +36,13 @@ module.exports = {
         .setDescription("The person you tag may be butthurt. Use at your own risk")
         .setRequired(true)),
     async execute(interaction) {
-        let author = getAuthor(String(interaction));
+        let author = getInsultedUser(interaction);
+        // if I am being insulted, don't
+        if (author == "652511543845453855") {
+            console.log("I am being insulted, this will not fly");
+            // get the user that ran the command and insult them instead
+            author = String(interaction.user.id);
+        }
         // let userMessage = interaction.options._hoistedOptions[0].value;
         let insult = await getInsult();
         await interaction.reply(`<@${author}> ${insult}`);
--- debate_dragon/src/ts/commands/insult.ts
+++ debate_dragon/src/ts/commands/insult.ts
@@ -1,12 +1,12 @@
-import { MessageEmbed } from "discord.js";
-const { SlashCommandBuilder } = require("@discordjs/builders");
+import { Interaction, MessageEmbed, User } from "discord.js";
+import { SlashCommandBuilder } from "@discordjs/builders";
 import axios from "axios";
 
-async function getInsult() {
-   /*
-    * Return an insult from an api
-    * @return {json} an insult
-    */
+/**
+ * gets an insult from the API
+ * @returns {Promise<string>} Returns the insult from the API
+ */
+async function getInsult(): Promise<string> {
    interface IInsult {
       status: number;
       statusText: string;
@@ -17,12 +17,22 @@ async function getInsult() {
    let response: IInsult = await axios.get(
       "https://evilinsult.com/generate_insult.php?lang=en"
    );
+
    let insult: string = response.data;
+   // check if response contains invalid words
+   if (insult.includes("&quot;")) {
+      await getInsult();
+   }
    return insult;
 }
 
-function getAuthor(interaction: string) {
-   return interaction.split(":")[1];
+/**
+ * get the user that is being insulted
+ * @param interaction - the interaction object
+ * @returns the id of the user tagged in the message
+ */
+function getInsultedUser(interaction: Interaction): string {
+   return String(interaction).split(":")[1];
 }
 
 module.exports = {
@@ -38,8 +48,14 @@ module.exports = {
             .setRequired(true)
       ),
 
-   async execute(interaction: any) {
-      let author = getAuthor(String(interaction));
+   async execute(interaction: Interaction) {
+      let author = getInsultedUser(interaction);
+      // if I am being insulted, don't
+      if (author == "652511543845453855") {
+         console.log("I am being insulted, this will not fly");
+         // get the user that ran the command and insult them instead
+         author = String(interaction.user.id);
+      }
       // let userMessage = interaction.options._hoistedOptions[0].value;
       let insult: string = await getInsult();
       await interaction.reply(`<@${author}> ${insult}`);
