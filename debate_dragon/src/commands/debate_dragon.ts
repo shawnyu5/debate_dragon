877252f0254dd5d9cf2dea22a90c0c493c98d6c9
shawn yu <shawn.yu5@hotmail.com>
2022-06-22 00:08:22 -0400

    update

--- debate_dragon/src/commands/debate_dragon.ts
+++ debate_dragon/src/commands/debate_dragon.ts
@@ -15,6 +15,7 @@ module.exports = {
       await interaction.deferReply();
       let userMessage = interaction.options.get("message")?.value as string;
       console.log("execute userMessage: %s", userMessage); // __AUTO_GENERATED_PRINT_VAR__
+      console.log("execute#if userMessage.length: %s", userMessage.length); // __AUTO_GENERATED_PRINT_VAR__
       if (userMessage.length > 20) {
          console.log("userMessage too long, cutting short");
          userMessage = userMessage.substring(0, 20);
