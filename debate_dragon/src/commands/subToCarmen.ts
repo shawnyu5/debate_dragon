3a0390d37593e1620447713b72424fe19a739c27
shawn yu <shawn.yu5@hotmail.com>
2022-06-23 13:20:24 -0400

    feat(subforcarmen): `sendNotification()`
    subforcarmen exports a function that sends a notification to users that
    subscribes to the rambles. It also keeps track of cool down in a local
    sqlite db, and will only send notifications every hour

--- debate_dragon/src/commands/subToCarmen.ts
+++ debate_dragon/src/commands/subToCarmen.ts
@@ -3,6 +3,9 @@ import { Client, CommandInteraction } from "discord.js";
 import { writeToConfig } from "../utils";
 import { QuickDB } from "quick.db";
 import { IConfig } from "../types/config";
+import { getChannelByName } from "../utils";
+
+const db = new QuickDB();
 
 module.exports = {
    data: new SlashCommandBuilder()
@@ -58,8 +61,49 @@ module.exports = {
     * @param client - discord client to send the message too
     */
    async sendNotification(client: Client) {
-      const db = new QuickDB();
-      let cooldown = 3.6e6; // 1 hour in milliseconds
-      await db.set("carmenCooldown", cooldown);
+      const config: IConfig = require("../../config.json");
+      const dbLabel = "carmenLastNotification";
+      const notificationUsers = config.carmenRambles.subscribers;
+      const currentTime = new Date();
+
+      const channelToSend = getChannelByName(
+         client,
+         config.carmenRambles.channelName
+      );
+      // if not channel found, then its a config error most likely
+      if (!channelToSend) {
+         console.log(
+            "Please spesify the channel to send notifications in config.json"
+         );
+         return;
+      }
+      const lastNotificationTime: Date | null = await db.get(dbLabel);
+      // if less than 1 hour has passed since the last notification, do nothing
+      if (
+         lastNotificationTime &&
+         lastNotificationTime.getHours() - currentTime.getHours() < 1
+      ) {
+         // this means carmen is still rambling, so don't keep notifying user of this
+         // await db.set(dbLabel, currentTime);
+         return;
+      }
+      // set current notification time
+      await db.set(dbLabel, currentTime);
+      let message = this.constructNotification(notificationUsers);
+      channelToSend.send(message);
+   },
+
+   /**
+    * construct a notification message alerting the users about carmen's ramblings
+    * @param users - array of users to send the message too
+    * @returns message to send to the users
+    */
+   constructNotification(users: Array<string>): string {
+      let message = "";
+      users.forEach((user) => {
+         message += `<@${user}> `;
+      });
+      message += " carmen is Rambling now!!!";
+      return message;
    },
 };
