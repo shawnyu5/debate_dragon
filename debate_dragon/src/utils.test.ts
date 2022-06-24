e45fcaeae04d7a1ef11238244192dda88878fb31
shawn yu <shawn.yu5@hotmail.com>
2022-06-23 22:09:51 -0400

    feat: logger

--- debate_dragon/src/utils.test.ts
+++
@@ -1,7 +0,0 @@
-import { removeCommandPrefix, constructCommandArgs } from "./utils";
-import { Interaction } from "discord.js";
-
-test("should remove command from message", () => {
-   let result = removeCommandPrefix("$dd", "$dd hello world");
-   expect(result).toMatch(/hello world/);
-});
