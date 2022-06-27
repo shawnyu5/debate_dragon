ca9525e733b3684a619766193863b34efc2b5902
shawn yu <shawn.yu5@hotmail.com>
2022-06-27 11:57:42 -0400

    feat(subToCarmen): Change subscription implementation to use roles
    Instead of storing subscribers in local config

--- debate_dragon/src/types/config.ts
+++ debate_dragon/src/types/config.ts
@@ -12,6 +12,10 @@ export interface IConfig {
       guildID: string;
       // the number of messages to trigger a notification
       messageLimit: number;
+      // carmen subscriber role id. The role to ping when she starts rambling
+      subscribersRoleID: "991000363408719953";
+      // number of minutes to wait before sending a notification
+      coolDown: 60;
    };
    logLevel: "debug" | "info" | "warn" | "error";
    development: boolean;
